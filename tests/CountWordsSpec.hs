f4cc22cc8fa28d11e7d79670536bbddc5d7984cd
Arialdo Martini <arialdomartini@gmail.com>
2022-11-14 22:05:38 +0100

    Find the most frequent word in a text, removing non letters

--- tests/CountWordsSpec.hs
+++ tests/CountWordsSpec.hs
@@ -1,7 +1,9 @@
 module CountWordsSpec where
 
 import Test.Hspec
-import Data.List (sortOn)
+import Data.List (sortOn, group)
+import Data.Char (isLetter)
+import GHC.OldList (sort)
 
 countWords :: String -> Int
 countWords =
@@ -23,6 +25,12 @@ mostFrequentWord =
   where aggregate word frequencies = updateFrequency frequencies word
 
 
+removeNonLetters :: String -> String
+removeNonLetters = takeWhile isLetter . dropWhile  (not . isLetter)
+
+mostFrequentWord' :: String -> (String, Int)
+mostFrequentWord' = last . sortOn snd . fmap (\e -> (head e, length e)) . group . sort . fmap removeNonLetters . words
+
 spec :: Spec
 spec = do
   it "counts the number of unique words used (size of the vocabulary)" $ do
@@ -34,7 +42,10 @@ spec = do
     updateFrequency [("joe", 2), ("mary", 1)] "joe"  `shouldBe`  [("joe", 3), ("mary", 1)]
     updateFrequency [("joe", 2), ("mary", 1)] "mary" `shouldBe`  [("joe", 2), ("mary", 2)]
 
-
   it "calculates the most frequent word" $ do
     let word = mostFrequentWord "hey joe joe hey joe have a nice nice day joe" in
+      word `shouldBe` ("joe", 4)
+
+  it "calculates the most frequent word, ignoring non letters" $ do
+    let word = mostFrequentWord' "hey joe!! joe, hey joe! have a nice (nice) day joe!!" in
       word `shouldBe` ("joe", 4)
