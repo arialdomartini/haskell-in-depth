9a6650e2ae328d9075fb3a33df97af46975d0e98
Arialdo Martini <arialdomartini@gmail.com>
2022-11-17 22:32:34 +0100

    Comparison as a separate function

--- tests/OrderingSpec.hs
+++ tests/OrderingSpec.hs
@@ -3,10 +3,13 @@ module OrderingSpec where
 import Test.Hspec
 import Data.List (sortBy)
 
+compareNumbers :: Int -> Int -> Ordering
+compareNumbers a b = if a > b then GT else LT
+
 sorted :: [Int] -> [Int]
-sorted = sortBy (\a b -> if a > b then GT else LT)
+sorted = sortBy compareNumbers
 
 spec :: Spec
 spec = do
-  it "sort an array" $ do
+  it "sorts an array" $ do
     sorted [5,3,1,2,4] `shouldBe` [1,2,3,4,5]
