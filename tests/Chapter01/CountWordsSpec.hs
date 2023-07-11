0cf59d380443459737d301fd71933ae9aff37e6c
Arialdo Martini <arialdomartini@gmail.com>
2023-07-11 19:03:02 +0200

    Reads and writes from a real text file

--- tests/Chapter01/CountWordsSpec.hs
+++ tests/Chapter01/CountWordsSpec.hs
@@ -1,9 +1,10 @@
 module Chapter01.CountWordsSpec(spec) where
 
 import Test.Hspec
--- import qualified Data.Text as T (Text, words, pack, sort)
+import qualified Data.Text as T (Text, pack, unpack)
 import Data.List(group, sort, sortBy)
-
+import qualified Data.Text.IO as F
+import System.Directory
 
 countWords :: String -> Int
 countWords = length . words
@@ -17,6 +18,15 @@ stats :: String -> [String]
 stats s = reverse (fmap fst ordered)
   where ordered = sortBy (\a b -> compare (snd a) (snd b)) (countOccurrencesOfWords s) 
 
+
+-- statsFromFile :: String -> IO ([String])
+statsFromFile path = do
+  text <- F.readFile path
+  let s = stats (T.unpack text)
+  return s
+
+
+
 spec :: Spec
 spec = do
   it "counts the words in a string" $ do
@@ -27,3 +37,14 @@ spec = do
 
   it "sorts the words in order of occurrences" $ do
     stats "first first last first last first second second second first first first" `shouldBe` ["first", "second", "last"]
+
+  
+  it "works on a real text file" $ do
+    F.writeFile path text
+    s <- statsFromFile path
+
+    s `shouldBe` ["one", "two", "three"]
+
+    System.Directory.removeFile path
+    where path = "count-words-test-file.txt"
+          text = T.pack "two two two one three one one one one one one one"
