e182b419857917d02befd72d48285b3d8419ce32
Arialdo Martini <arialdomartini@gmail.com>
2023-07-11 09:40:56 +0200

    Counts occurrences of words

--- tests/Chapter01/CountWordsSpec.hs
+++ tests/Chapter01/CountWordsSpec.hs
@@ -1,12 +1,21 @@
 module Chapter01.CountWordsSpec(spec) where
 
 import Test.Hspec
-import qualified Data.Text as T (words, pack)
+-- import qualified Data.Text as T (Text, words, pack, sort)
+import Data.List(group, sort)
 
 countWords :: String -> Int
-countWords = length . T.words . T.pack
+countWords = length . words
+
+countOccurrencesOfWords :: String -> [(String, Int)]
+countOccurrencesOfWords s =
+  fmap tup $ (group . sort . words) s
+  where tup e = (head e, length e)
 
 spec :: Spec
 spec = do
   it "counts the words in a string" $ do
     countWords "one two three, four five-cinque (six)" `shouldBe` 6
+
+  it "counts the occurrences of words" $ do
+    countOccurrencesOfWords "one two one one two" `shouldBe` [("one", 3), ("two", 2)]
